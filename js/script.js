7918442f21f8bd12bb9aa09c948a4c81534aa40e
carlosmsb <carlosmsb94@gmail.com>
2023-09-24 20:56:07 -0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 //Convert the following identifiers to Camel Case notation:
 /*
-let some_month = let someMonth
-function the Month() = function theMonth()
-let current-month = let currentMonth
-let summer_month = let summerMonth
+let some_month = let someMonth;
+function the Month() = function theMonth();
+let current-month = let currentMonth;
+let summer_month = let summerMonth;
 let MyLibrary-function = let myLibraryFunction
 */
 
 //Give an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression.
 /*
-numericLiteral = 2.0
-stringLiteral = "Hello World"
-Boolean literal = true
+numericLiteral = 2.0;
+stringLiteral = "Hello World";
+Boolean literal = true;
 null literal = null
 */
 
@@ -25,4 +25,27 @@ let age = 29;
 let isAdult = age >= 18 ? "Yes" : "No";
 //Get result for isAdult
 console.log(isAdult)
+*/
+
+//Declare 9 variables for the following identifiers. Use Camel Casing and Hungarian Notation: 
+/*
+const firstName;
+const lastName;
+let Address;
+let City;
+let State;
+var intZipCode;
+var intYourAge;
+let referralSource; 
+let mayWeContactYou
+*/
+
+//Take the 3 of the 9 variables created above and demonstrate 3 ways for declaring and assigning values to those variables.
+/*
+//using Const:
+const firstName = Carlos;
+//using Let:
+let State = CA;
+//using Hungarian Notation:
+var intYourAge = 29;
 */
