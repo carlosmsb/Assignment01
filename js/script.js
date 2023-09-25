5e825aae913e9766208ea3dad6a2f7bad9d63a71
carlosmsb <carlosmsb94@gmail.com>
2023-09-24 19:58:28 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//Convert the following identifiers to Camel Case notation:
+/*
+let some_month = let someMonth
+function the Month() = function theMonth()
+let current-month = let currentMonth
+let summer_month = let summerMonth
+let MyLibrary-function = let myLibraryFunction
+*/
+
+//Give an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression.
+/*
+numericLiteral = 2.0
+stringLiteral = "Hello World"
+Boolean literal = true
+null literal = null
+*/
+
+//Give two examples of complex / variable expressions.
+/*
+let complex = 3 * 5 + 2;
+//Get result for complex variable
+console.log(complex)
+
+let age = 29;
+let isAdult = age >= 18 ? "Yes" : "No";
+//Get result for isAdult
+console.log(isAdult)
+*/
